be741233b05bbe71d443dda14891b2b5d2557844
xanvranck <vranckaert.xan@gmail.com>
2019-08-30 18:29:14 +0200

    countdown shizzle

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { InfoComponent } from './info/info.component';
+import { CountdownComponent } from './countdown/countdown.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HeaderComponent },
+  { path: 'home', component: HeaderComponent },
+  { path: 'info', component: InfoComponent },
+  { path: 'countdown', component: CountdownComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
