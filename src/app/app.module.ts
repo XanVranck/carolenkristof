2e9aab18aaa4235257c083d6c183bb31a7b32373
xanvranck <vranckaert.xan@gmail.com>
2019-08-30 19:11:04 +0200

    added hints component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { InfoComponent } from './info/info.component';
 import { CountdownComponent } from './countdown/countdown.component';
+import { HintsComponent } from './hints/hints.component';
 
 
 @NgModule({
@@ -13,7 +14,8 @@ import { CountdownComponent } from './countdown/countdown.component';
     AppComponent,
     HeaderComponent,
     InfoComponent,
-    CountdownComponent
+    CountdownComponent,
+    HintsComponent
   ],
   imports: [
     BrowserModule,
