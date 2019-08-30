d7505b12693f6f5b16d5151654fd889e7e146f4a
xanvranck <vranckaert.xan@gmail.com>
2019-08-30 16:45:25 +0200

    headercomponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import {CountDown} from "ng2-date-countdown";
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    CountDown
   ],
   imports: [
     BrowserModule,
